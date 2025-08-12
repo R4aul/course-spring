abd2f8cf8d9df59e63260823a3482f76e4f51578
Raul Damian Rafael <raul.damian220@gmail.com>
2025-08-11 21:51:42 -0600

    feat : Class 18 | Product Repository

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.ProductoEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductoRepository {
 
     public Optional<List<ProductoEntity>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<ProductoEntity> getProducto(int id){
+        return productoCrudRepository.findById(id);
+    }
+
+    public ProductoEntity save(ProductoEntity producto){
+       return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
